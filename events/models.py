2db52907d3c7e83db29a11fb83b504adc6aacab3
Melvin-Thomas-Dev <57299818+Melvin-Thomas-Dev@users.noreply.github.com>
2020-05-19 05:04:47 +0530

    events model

--- events/models.py
+++ events/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from imagekit.models import ProcessedImageField
 
+
+event_type_choices = (
+    ('Competition','competition'),
+    ('workshop','workshop'),
+    ('webinar','webinar'),
+    ('talk session','talk session'),
+    ('peer to peer','peer to peer'),
+    ('miscellaneous','miscellaneous'),
+    ('other','other')
+)
 # Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=150, blank=False)
+    event_type = models.CharField(max_length=200,choices=event_type_choices)
+    slug = models.SlugField(max_length=200, blank=False, unique=True, default='slug')
+    brief = models.CharField(max_length=400, blank=True)
+    description = models.TextField(blank=False, default=' ')
+    date = models.CharField(max_length=100, blank=False)
+    fees = models.TextField(max_length=50, blank=True)
+    prize = models.TextField(max_length=50, blank=True)
+    cover = ProcessedImageField(upload_to='post_images', blank=False)
+
